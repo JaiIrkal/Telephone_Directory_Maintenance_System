bc516b89292f55f630b238f724b62314d4feb63e
Ankit <50232341+imankit007@users.noreply.github.com>
2022-09-20 15:11:49 +0530

    Update DeptMaint-1.c

--- DeptMaint-1.c
+++ DeptMaint-1.c
@@ -1,15 +1,14 @@
 /******************************************************************************
-* Filename    : DeptMaint.c
-* Author      : Education & Research Dept, Infosys Technologies Limited
-* Date        : 11-Mar-2009
-* Description : Contains the functions required for the department 
-*		maintenance.
-*******************************************************************************/
-
+ * Filename    : DeptMaint.c
+ * Author      : Education & Research Dept, Infosys Technologies Limited
+ * Date        : 11-Mar-2009
+ * Description : Contains the functions required for the department
+ *		maintenance.
+ *******************************************************************************/
 
 /* Include files */
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <conio.h>
 
@@ -22,53 +21,52 @@ int iFlag;
 int iFlag2;
 
 /******************************************************************************
-* Function: fnInitializeDeptCode                 	
-* Description: Reads all the departments in the department file and initializes
-*		the global variable giDeptCode for any new department to be added to
-*		the system. The Dept file should've been opened in the program already!
-* Input Parameters: None
-******************************************************************************/
+ * Function: fnInitializeDeptCode
+ * Description: Reads all the departments in the department file and initializes
+ *		the global variable giDeptCode for any new department to be added to
+ *		the system. The Dept file should've been opened in the program already!
+ * Input Parameters: None
+ ******************************************************************************/
 
-void fnInitializeDeptCode
+void fnInitializeDeptCode()
 {
 }
 
 /******************************************************************************
-* Function: fnAddDept
-* Description: Adds a new department entry into the Dept file
-*		The Dept file should've been opened in the program already!
-* Input Parameters: None
-* 	char acDeptName[] - Name of the new dept
-* Returns: Zero on success. 1 if dept name exists already.
-******************************************************************************/
+ * Function: fnAddDept
+ * Description: Adds a new department entry into the Dept file
+ *		The Dept file should've been opened in the program already!
+ * Input Parameters: None
+ * 	char acDeptName[] - Name of the new dept
+ * Returns: Zero on success. 1 if dept name exists already.
+ ******************************************************************************/
 
-int fnAddDept (char acDeptName[])
+int fnAddDept(char acDeptName[])
 {
 }
 
 /******************************************************************************
-* Function: fnPrintDepts
-* Description: Prints all the department codes and names that exist in the 
-*		file dept.txt
-* Input Parameters: None
-******************************************************************************/
+ * Function: fnPrintDepts
+ * Description: Prints all the department codes and names that exist in the
+ *		file dept.txt
+ * Input Parameters: None
+ ******************************************************************************/
 
-void fnPrintDepts ()
+void fnPrintDepts()
+{
+    fnOpenFile(1);
+}
+
+/******************************************************************************
+ * Function: fnAddNewDepartment
+ * Description: Allows user to visually add a new department into the system
+ * Input Parameters: None
+ ******************************************************************************/
+
+void fnAddNewDepartment()
 {
 }
 
-
 /******************************************************************************
-* Function: fnAddNewDepartment
-* Description: Allows user to visually add a new department into the system
-* Input Parameters: None
-******************************************************************************/
-
-void fnAddNewDepartment ()
-{
-}
-
-
-/******************************************************************************
-* End of DeptMaint.c
-******************************************************************************/
+ * End of DeptMaint.c
+ ******************************************************************************/
