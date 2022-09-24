1eaeeed3ddd0f669aac1f4afe193ea0ca7361bd7
Ankit <50232341+imankit007@users.noreply.github.com>
2022-09-24 21:46:52 +0530

    Co-authored-by: Jai Irkal <JaiIrkal@users.noreply.github.com>

--- TeleDir.c
+++ TeleDir.c
@@ -1,16 +1,16 @@
 /******************************************************************************
-* Filename    : TelDir.c
-* Author      : E&R, Infosys Technologies Limited
-* Date        : 04-July-2005
-* Description : Displays the main menu for telephone directory maintenace and
-*				invokes the respective functions
-******************************************************************************/
+ * Filename    : TelDir.c
+ * Author      : E&R, Infosys Technologies Limited
+ * Date        : 04-July-2005
+ * Description : Displays the main menu for telephone directory maintenace and
+ *				invokes the respective functions
+ ******************************************************************************/
 
 /* Include files */
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <conio.h>`
 
 /* Declare constants and function prototypes */
 #include "filefunctions.h"
@@ -19,7 +19,7 @@
 int fnInitializeDeptCode();
 int fnAddDept(char acDeptName[]);
 void fnAddNewDepartment();
-void fnPrintDepts ();
+void fnPrintDepts();
 /* Forward declaration of functions defined in EmpMaint.c */
 int fnInitializeEmpCode();
 void fnAddNewEmp();
@@ -30,183 +30,159 @@ void fnEnqByTeleNum();
 void fnEnqByName();
 
 /*Function prototype*/
-void fnMainMenu (); 
+void fnMainMenu();
 
 void fnTeleDirMenu();
 void fnLoginValidate(char acUserId[]);
 
 /******************************************************************************
-* Function: fnDisplayTeleDirMenu
-* Description: Displays the Telephone Directory Maintenance menu. Waits for user's
-*		input in keyboard using scanf. Returns control back to calling function
-*		only after user has typed a choice.
-* Input Parameters: None
-* Returns: The user's choice.
-*******************************************************************************/
+ * Function: fnDisplayTeleDirMenu
+ * Description: Displays the Telephone Directory Maintenance menu. Waits for user's
+ *		input in keyboard using scanf. Returns control back to calling function
+ *		only after user has typed a choice.
+ * Input Parameters: None
+ * Returns: The user's choice.
+ *******************************************************************************/
 
-int fnDisplayTeleDirMenu() 
+int fnDisplayTeleDirMenu()
 {
-	
+
 	printf("\n");
 	printf("Telephone Directory Mainatinance System\n");
 	printf("========================================");
-
 }
 
-
 /******************************************************************************
-* Function: fnDisplayTeleEnqMenu
-* Description: Displays the Telephone Enquiry menu. Waits for user's
-*		input in keyboard using scanf. Returns control back to calling function
-*		only after user has typed a choice.
-* Input Parameters: None
-* Returns: The user's choice.
-******************************************************************************/
+ * Function: fnDisplayTeleEnqMenu
+ * Description: Displays the Telephone Enquiry menu. Waits for user's
+ *		input in keyboard using scanf. Returns control back to calling function
+ *		only after user has typed a choice.
+ * Input Parameters: None
+ * Returns: The user's choice.
+ ******************************************************************************/
 
 int fnDisplayTeleEnqMenu()
 {
-	
 }
-
-
 
 /******************************************************************************
-* Function: fnTeleEnqMenu
-* Description: Handles the Telephone Enquiry menu. Calls fnDisplayTeleEnqMenu
-*		function to display the menu. Choice of user is handled by this 
-*		function.
-* Input Parameters: None
-* Returns: None
-******************************************************************************/
-void fnTeleEnqMenu() 
+ * Function: fnTeleEnqMenu
+ * Description: Handles the Telephone Enquiry menu. Calls fnDisplayTeleEnqMenu
+ *		function to display the menu. Choice of user is handled by this
+ *		function.
+ * Input Parameters: None
+ * Returns: None
+ ******************************************************************************/
+void fnTeleEnqMenu()
 {
-	
 }
-
 
 /******************************************************************************
 * Function: fnTeleDirMenu
-* Description: Handles the Telephone Directory Maintenance menu. Calls 
-	fnDisplayTeleDirMenu function to display the menu. Choice of user is 
+* Description: Handles the Telephone Directory Maintenance menu. Calls
+	fnDisplayTeleDirMenu function to display the menu. Choice of user is
 	handled by this	function.
 * Input Parameters: None
 * Returns: None
 ******************************************************************************/
 
-void fnTeleDirMenu() 
+void fnTeleDirMenu()
 {
-	
 }
-
-
 
 /******************************************************************************
-* Function: fnDisplayEmpMenu
-* Description: Displays the Employee Maintenance menu. Waits for user's
-*		input in keyboard using scanf. Returns control back to calling function
-*		only after user has typed a choice.
-* Input Parameters: None
-* Returns: The user's choice.
-******************************************************************************/
+ * Function: fnDisplayEmpMenu
+ * Description: Displays the Employee Maintenance menu. Waits for user's
+ *		input in keyboard using scanf. Returns control back to calling function
+ *		only after user has typed a choice.
+ * Input Parameters: None
+ * Returns: The user's choice.
+ ******************************************************************************/
 
-int fnDisplayEmpMenu() 
+int fnDisplayEmpMenu()
 {
-	
 }
-
 
 /******************************************************************************
-* Function: fnDisplayDepartmentMenu
-* Description: Displays the Department Maintenance menu. Waits for user's
-*		input in keyboard using scanf. Returns control back to calling function
-*		only after user has typed a choice.
-* Input Parameters: None
-* Returns: The user's choice.
-******************************************************************************/
+ * Function: fnDisplayDepartmentMenu
+ * Description: Displays the Department Maintenance menu. Waits for user's
+ *		input in keyboard using scanf. Returns control back to calling function
+ *		only after user has typed a choice.
+ * Input Parameters: None
+ * Returns: The user's choice.
+ ******************************************************************************/
 
-int fnDisplayDepartmentMenu() 
+int fnDisplayDepartmentMenu()
 {
-	
 }
-
 
 /******************************************************************************
-* Function: fnEmpMenu
-* Description: Handles the Employee Maintenance menu. Calls fnDisplayEmpMenu
-*		function to display the menu. Choice of user is handled by this 
-*		function.
-* Input Parameters: None
-* Returns: None
-******************************************************************************/
+ * Function: fnEmpMenu
+ * Description: Handles the Employee Maintenance menu. Calls fnDisplayEmpMenu
+ *		function to display the menu. Choice of user is handled by this
+ *		function.
+ * Input Parameters: None
+ * Returns: None
+ ******************************************************************************/
 
-void fnEmpMenu() 
+void fnEmpMenu()
 {
-	
 }
-
 
 /******************************************************************************
-* Function: fnDepartmentMenu
-* Description: Handles the Department menu. Calls fnDisplayDepartmentMenu
-*		function to display the menu. Choice of user is handled by this 
-*		function.
-* Input Parameters: None
-* Returns: None
-******************************************************************************/
+ * Function: fnDepartmentMenu
+ * Description: Handles the Department menu. Calls fnDisplayDepartmentMenu
+ *		function to display the menu. Choice of user is handled by this
+ *		function.
+ * Input Parameters: None
+ * Returns: None
+ ******************************************************************************/
 
-void fnDepartmentMenu() 
+void fnDepartmentMenu()
 {
-	
 }
-
 
 /******************************************************************************
-* Function: fnDisplayMainMenu
-* Description: Displays the main menu of telephone directory system. Then
-*		waits for user's input using scanf function. Until the user types in
-*		a selection, control will not be returned to the calling function.
-* Input Parameters: None
-* Returns: The user's choice
-******************************************************************************/
+ * Function: fnDisplayMainMenu
+ * Description: Displays the main menu of telephone directory system. Then
+ *		waits for user's input using scanf function. Until the user types in
+ *		a selection, control will not be returned to the calling function.
+ * Input Parameters: None
+ * Returns: The user's choice
+ ******************************************************************************/
 
-int fnDisplayMainMenu() 
+int fnDisplayMainMenu()
 {
-
-	
 }
-
-
 
 /******************************************************************************
-* Function: fnMainMenu
-* Description: Calls fnDisplayMainMenu and waits for user's choice. Upon
-*     user's selection, does appropriate action. If user chooses 'exit'
-*     option, returns control back to the calling function.
-* Input Parameters: None
-* Returns : None
-******************************************************************************/
+ * Function: fnMainMenu
+ * Description: Calls fnDisplayMainMenu and waits for user's choice. Upon
+ *     user's selection, does appropriate action. If user chooses 'exit'
+ *     option, returns control back to the calling function.
+ * Input Parameters: None
+ * Returns : None
+ ******************************************************************************/
 
-void fnMainMenu () 
+void fnMainMenu()
 {
-	
 }
-
 
 /******************************************************************************
 * Function: fnLogin
 Description: Check for valid User Id and calls fnMainMenu.
- If invalid employee id is entered for User Id, error message "Login Denied" 
+ If invalid employee id is entered for User Id, error message "Login Denied"
  is displayed and quits the application.
 * Input Parameters: None
 * Return : None
 ******************************************************************************/
-void fnLogin(){
+void fnLogin()
+{
 
 	fnLoginScreen();
 }
 
-
-void fnLoginScreen() 
+void fnLoginScreen()
 {
 
 	char chUserID[10];
@@ -217,34 +193,26 @@ void fnLoginScreen()
 	printf("			============			 \n");
 	printf("Enter User ID 	:  ......");
 	scanf("%s", chUserID);
-	
 }
 void fnLoginValidate(char acUserId[])
 {
-    
 }
 
-
-
 /******************************************************************************
-* Function: main
-* Description: Entry point of program.
-* Input Parameters: None
-******************************************************************************/
+ * Function: main
+ * Description: Entry point of program.
+ * Input Parameters: None
+ ******************************************************************************/
 
-int main (int argc, char** argv) 
+int main(int argc, char **argv)
 {
 	/* calling fnLogin function to generate login screen*/
 	fnLogin();
 
-
-	/* Return a success code to Operating System */ 
+	/* Return a success code to Operating System */
 	return 0;
 }
 
 /******************************************************************************
-* End of TelDir.c
-******************************************************************************/
-
-
-
+ * End of TelDir.c
+ ******************************************************************************/
