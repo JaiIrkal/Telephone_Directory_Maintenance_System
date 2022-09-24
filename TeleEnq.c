326393d76b42e6857a4f89f4fc2322fc5d9c9630
Ankit <50232341+imankit007@users.noreply.github.com>
2022-09-24 12:03:27 +0530

    added new files

---
+++ TeleEnq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void fnEnqByTeleNum();
+void fnEnqByName();
+
+void fnEnqByTeleNum()
+{
+    printf("Telephone Directory Maintenance System\n");
+    printf("======================================\n");
+    printf("Telephone Enquiry by Name\n");
+    printf("================\n");
+}
+void fnEnqByName()
+{
+    printf("Telephone Directory Maintenance System\n");
+    printf("======================================\n");
+    printf("Telephone Enquiry by Number\n");
+    printf("================\n");
+}
